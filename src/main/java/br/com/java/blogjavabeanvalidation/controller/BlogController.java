58e31a6de16f611e1a89ec0d72ec617cdf3e3bd5
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-01-05 17:28:26 -0300

    criação dos métodos de lista e falha de validação.

--- src/main/java/br/com/java/blogjavabeanvalidation/controller/BlogController.java
+++ src/main/java/br/com/java/blogjavabeanvalidation/controller/BlogController.java
@@ -1,10 +1,16 @@
 package br.com.java.blogjavabeanvalidation.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
 
 import br.com.java.blogjavabeanvalidation.model.Blog;
 import br.com.java.blogjavabeanvalidation.repository.BlogRepository;
@@ -16,6 +22,12 @@ public class BlogController {
     @Autowired
     private BlogRepository blogRepository;
 
+    @GetMapping("/blogs")
+    public List<Blog> findAll() {
+
+        return (List<Blog>) blogRepository.findAll();
+    }
+
     @PostMapping("/blog")
     @ResponseStatus(HttpStatus.CREATED)
     public Blog salvarBlog(@Valid @RequestBody Blog blog) {
@@ -23,4 +35,14 @@ public class BlogController {
         return blogRepository.save(blog);
     }
     
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValid(MethodArgumentNotValidException  ex) {
+        Map<String, String> erros = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> 
+              erros.put(error.getField(), error.getDefaultMessage()));
+
+              return erros;
+    }
 }
